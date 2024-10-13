72499500c7a5ad05f6421b4a6bd31bc9c1862186
Kranimo <145914094+Kranimo@users.noreply.github.com>
2024-10-13 21:16:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+dictionary = {
+    "Motherboard": "The part of the computer that makes other components communicate.",
+    "python": "A coding language",
+    "java": "Another coding language",
+    "ram": "A component that acts like temporary storage area which programs run.",
+    "linux": "An open source operating system kernel(core)",
+    "html": "Hypertext markup language"
+}
+
+a = input("Which term are you trying to search up?\n")
+print(dictionary[a])
